0f8a4a52ff512738bcb25fa4c56ee11b88842432
cdvincoy <99915734+cdvincoy@users.noreply.github.com>
2022-03-08 16:17:03 +0800

    Add files via upload

---
+++ CMSC21/Lecture 1/Assignment/assignment_lec1_c1.c
@@ -0,0 +1,15 @@
+//This is a program that shows several sentences about C programming.
+
+#include <stdio.h> //This line is always present in making a program using C.
+int main(void)
+{
+	//Here, the four sentences are inputted. Using the prinft function, they will appear once the program runs.
+	printf("a. In C, lowercase letters are significant.\n"); //\n is used so that it will proceed to the next line, just like in the example shown.
+	printf("b. main is where program execution begins.\n");
+	printf("c. Opening and closing braces enclose program statements in a routine.\n");
+	printf("d. All program statements must be terminated by a semicolon.\n");
+	return 0; //The program ends here.
+}
+
+			
+	
